31e340747b8e466d2b454721d2f5210ad530c7db
HAYBISH10 <hassanissackmohamed172@gmail.com>
2025-03-10 22:55:50 +0300

    final update

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
+const returnFirstTwoDrivers = drivers => drivers.slice(0, 2);
 
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0, 2);
-};
-
-const returnLastTwoDrivers = function(drivers) {
-    return drivers.slice(-2);
-};
-
+const returnLastTwoDrivers = drivers => drivers.slice(-2);
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-
-const createFareMultiplier = function(multiplier) {
-    return function(fare) {
-        return fare * multiplier;
-    };
-};
-
+const createFareMultiplier = multiplier => fare => fare * multiplier;
 
 const fareDoubler = createFareMultiplier(2);
 
-
 const fareTripler = createFareMultiplier(3);
 
-const selectDifferentDrivers = function(drivers, driverFunction) {
-    return driverFunction(drivers);
-};
-
-if (typeof module !== 'undefined' && module.exports) {
-    module.exports = {
-        returnFirstTwoDrivers,
-        returnLastTwoDrivers,
-        selectingDrivers,
-        createFareMultiplier,
-        fareDoubler,
-        fareTripler,
-        selectDifferentDrivers
-    };
-}
-
+const selectDifferentDrivers = (drivers, driverFunction) => driverFunction(drivers);
